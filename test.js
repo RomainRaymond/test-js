3188cb1c0bf2862de3c520028208abb430956b3b
Romain Richaud <romainrichaud6@gmail.com>
2021-03-30 23:58:47 +0200

    premier envoi sur github

---
+++ test.js
@@ -0,0 +1,13 @@
+console.log("Bonjour");
+
+for (i=0; i < 3; i++) {
+    console.log(i);
+}
+
+console.log("Au revoir")
+
+for (i=10; i > 0; i--) {
+    console.log(i);
+}
+
+console.log("Re au revoir");
